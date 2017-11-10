f70f7ecb56161ad8fecb2e20f9fad33a3c0941b2
CSharpFri <CSharpFri@CLASS-2-12>
2017-11-10 21:19:52 +0100

    Obrazce

--- Obrazce/Program.cs
+++ Obrazce/Program.cs
@@ -10,6 +10,39 @@ namespace Obrazce
     {
         static void Main(string[] args)
         {
+            //Stvorec stvorec = new Stvorec(4.2);
+            //Console.WriteLine("Obsah stvorca: {0}",stvorec.VypocitajObsah());
+
+            //Stvorec stvorec2 = new Stvorec(4.2);
+
+            //Console.WriteLine("Porovnanie {0}", Stvorec.Porovnaj(stvorec, stvorec2));
+            
+            
+
+            //Obrazec kruh = new Kruh(3.4); //ked zmenime Obrazec na Kruh tak pomocou . mame ine funkcionality
+            
+            //Console.WriteLine("Obsah kruhu: {0}", kruh.VypocitajObsah());
+            
+            
+
+            //IKreslenie k = stvorec2;
+            //IKreslenie k1 = new Kruh(10.2);
+            //k.Vykresli();
+            //k1.Vytlac();
+
+            Kruh kruh1 = new Kruh(3);
+            IZmenRozmer zmenaKruhu = kruh1;
+            zmenaKruhu.ZmenRozmer(1);
+            Console.WriteLine("Obsah je: {0}",kruh1.VypocitajObsah()); 
+
+            Stvorec stvorec3 = new Stvorec(4);
+            IZmenRozmer zmenaStvorca = stvorec3;
+            zmenaStvorca.ZmenRozmer(2);
+            Console.WriteLine("Obsah je: {0}",stvorec3.VypocitajObsah()); 
+
+
+
+            Console.ReadKey();
         }
     }
 }
